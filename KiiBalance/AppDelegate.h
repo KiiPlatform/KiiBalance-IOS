39c92099f24f03fc52c3c6c33dee40cd055d5e37
hrk fkm <fkm@fkmsoft.jp>
2015-11-11 14:09:15 +0900

    Initial Commit

---
+++ KiiBalance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KiiBalance
+//
+//  Created by fkm on 2015/11/11.
+//  Copyright © 2015年 kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KiiBalance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KiiBalance
+//
+//  Created by fkm on 2015/11/11.
+//  Copyright © 2015年 kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
